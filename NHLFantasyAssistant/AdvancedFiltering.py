d7ba616c7a51b09471e4cbb579539bbbfc0c5695
Dillon Johnson <djtopshot13@gmail.com>
2025-01-28 20:45:17 -0700

    Updating all the code with the new Money Puck CSV data files as well.

--- NHLFantasyAssistant/AdvancedFiltering.py
+++ NHLFantasyAssistant/AdvancedFiltering.py
@@ -1,4 +1,32 @@
 import pandas as pd
-df = pd.read_csv()
+lines_df = pd.read_csv("lines.csv")
+teams_df = pd.read_csv("teams.csv")
+lines_df.drop(columns=["lineId", "season"], inplace=True)
 
-print(df.info())
+lines_df["TOI/G"] = round(lines_df["icetime"] / lines_df["games_played"], 2)
+lines_df = lines_df.sort_values('TOI/G', ascending=False)
+
+def topIceTimeForwardLines():
+    topIceTimeLines("line", 25)
+
+def topIceTimeDefenseLines():
+    topIceTimeLines("pairing", 25)
+
+def topIceTimeLines(position, line_count):
+    print(f"Top {line_count} {"Forwards" if position == "line" else "Defensemen"} with Most Ice Time\n\n")
+    count = 0
+    for index, row in lines_df.iterrows():
+        if row['position'] == position:
+            if count == line_count:
+                print()
+                break
+            print(f"{count+1}. {row['team']} {row['name']} {row['TOI/G']} TOI/G \n")
+            count += 1
+        else: 
+            continue
+    
+topIceTimeForwardLines()
+topIceTimeDefenseLines()
+
+# for col in lines_df.columns:
+#     print(col)
