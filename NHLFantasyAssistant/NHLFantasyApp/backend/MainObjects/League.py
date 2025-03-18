daf6792ac5c8a616a86ad3ded412136c6a683566
djtopshot13 <djtopshot13@gmail.com>
2025-03-18 12:56:31 -0600

    Revert "I got the data for the teams games played from the API, so I should be able to utilize that for further analysis"
    This reverts commit d8f4fa014900262586fdf32d7b9cf99c43075c1e.

--- NHLFantasyAssistant/NHLFantasyApp/backend/MainObjects/League.py
+++ NHLFantasyAssistant/NHLFantasyApp/backend/MainObjects/League.py
@@ -506,9 +506,6 @@ class League:
     for the 7 day window. Display each team from highest projected score to lowest projected score
     """
 
-    def getGamesPlayedByTeam(self, days=7):
-        for team in c
-
     def createLeague():
         # Initialize all necessary variables to be passed into League constructor
         _season_points = ESPNLeague._get_Season_Points()
