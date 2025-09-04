a0046a4fd5e62d40d705fa83f31ec3b1f5fece8d
djtopshot13 <djtopshot13@gmail.com>
2025-09-04 14:20:38 -0600

    Getting rid of old projects that aren't useful anymore

--- FirstTypeScript/README.md
+++
@@ -1,2 +0,0 @@
-# TypeScript Playground
-- This is a starter directory for me to learn and practice implementing code in TypeScript
--- FirstTypeScript/tsPractice/dist/index.js
+++
@@ -1,8 +0,0 @@
-let sales = 123_456_789;
-let course ="TypeScript";
-let is_published = true;
-let level;
-
-function render(document) {
-    console.log(document);
-}
